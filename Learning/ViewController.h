61b6226e059952380293848516503741cb10fc05
Jessica Craig <jesburt@ut.utm.edu>
2016-08-28 20:57:59 -0500

    Initial Commit

---
+++ Learning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Learning
+//
+//  Created by Jessica Craig on 8/28/16.
+//  Copyright © 2016 Jessica Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Learning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Learning
+//
+//  Created by Jessica Craig on 8/28/16.
+//  Copyright © 2016 Jessica Craig. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
