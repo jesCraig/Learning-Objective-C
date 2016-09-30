942e98d7a21b8d6c4ee2ef77c55f04cac2058759
Jessica Craig <jesburt@ut.utm.edu>
2016-09-30 09:53:22 -0500

    Constraints need work Drawing done - but not tested, b/c of constraints/viewing

--- Learning/ViewController.h
+++ Learning/ViewController.h
@@ -11,6 +11,15 @@
 @interface ViewController : UIViewController {
     IBOutlet UIImageView *imageView;
     NSMutableArray *imagesArray;
+    
+        
+    CGPoint lastPoint;
+    CGFloat red;
+    CGFloat green;
+    CGFloat blue;
+    CGFloat brush;
+    CGFloat opacity;
+    BOOL mouseSwiped;
 }
 
 
@@ -27,6 +36,15 @@
 - (IBAction)redButton:(id)sender;
 - (IBAction)orangeButton:(id)sender;
 - (IBAction)brownButton:(id)sender;
+- (IBAction)greyButton:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIImageView *mainView;
+@property (weak, nonatomic) IBOutlet UIImageView *tempView;
+
+- (IBAction)pencilPressed:(id)sender;
+- (IBAction)eraserPressed:(id)sender;
+- (IBAction)resetPressed:(id)sender;
+
 
 @end
 
--- Learning/ViewController.m
+++ Learning/ViewController.m
@@ -56,12 +56,71 @@
 
 }
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
+    red = 0.0/255.0;
+    green = 0.0/255.0;
+    blue = 0.0/255.0;
+    brush = 10.0;
+    opacity = 1.0;
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    imagesArray = [[NSMutableArray alloc] initWithObjects:@"yellowCard",@"pinkCard",@"blueCard",nil];
 }
 
+//Drawing Code
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    mouseSwiped = NO;
+    UITouch *touch = [touches anyObject];
+    lastPoint = [touch locationInView:self.view];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    mouseSwiped = YES;
+    UITouch *touch = [touches anyObject];
+    CGPoint currentPoint = [touch locationInView:self.view];
+    
+    UIGraphicsBeginImageContext(self.view.frame.size);
+    [self.tempView.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+    CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
+    CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brush );
+    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, 1.0);
+    CGContextSetBlendMode(UIGraphicsGetCurrentContext(),kCGBlendModeNormal);
+    
+    CGContextStrokePath(UIGraphicsGetCurrentContext());
+    self.tempView.image = UIGraphicsGetImageFromCurrentImageContext();
+    [self.tempView setAlpha:opacity];
+    UIGraphicsEndImageContext();
+    
+    lastPoint = currentPoint;
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    if(!mouseSwiped) {
+        UIGraphicsBeginImageContext(self.view.frame.size);
+        [self.tempView.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+        CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
+        CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brush);
+        CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, opacity);
+        CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+        CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
+        CGContextStrokePath(UIGraphicsGetCurrentContext());
+        CGContextFlush(UIGraphicsGetCurrentContext());
+        self.tempView.image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+    }
+    
+    UIGraphicsBeginImageContext(self.mainView.frame.size);
+    [self.mainView.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) blendMode:kCGBlendModeNormal alpha:1.0];
+    [self.tempView.image drawInRect:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) blendMode:kCGBlendModeNormal alpha:opacity];
+    self.mainView.image = UIGraphicsGetImageFromCurrentImageContext();
+    self.tempView.image = nil;
+    UIGraphicsEndImageContext();
+}
 
 
 
@@ -92,6 +151,9 @@
 - (IBAction)brownButton:(id)sender {
 }
 
+- (IBAction)greyButton:(id)sender {
+}
+
 
 
 - (void)didReceiveMemoryWarning {
@@ -99,4 +161,73 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)pencilPressed:(id)sender {
+        
+        UIButton * PressedButton = (UIButton*)sender;
+        
+        switch(PressedButton.tag)
+        {
+            case 0:
+                red = 0.0/255.0;
+                green = 0.0/255.0;
+                blue = 0.0/255.0;
+                break;
+            case 1:
+                red = 105.0/255.0;
+                green = 105.0/255.0;
+                blue = 105.0/255.0;
+                break;
+            case 2:
+                red = 255.0/255.0;
+                green = 0.0/255.0;
+                blue = 0.0/255.0;
+                break;
+            case 3:
+                red = 0.0/255.0;
+                green = 0.0/255.0;
+                blue = 255.0/255.0;
+                break;
+            case 4:
+                red = 102.0/255.0;
+                green = 204.0/255.0;
+                blue = 0.0/255.0;
+                break;
+            case 5:
+                red = 102.0/255.0;
+                green = 255.0/255.0;
+                blue = 0.0/255.0;
+                break;
+            case 6:
+                red = 51.0/255.0;
+                green = 204.0/255.0;
+                blue = 255.0/255.0;
+                break;
+            case 7:
+                red = 160.0/255.0;
+                green = 82.0/255.0;
+                blue = 45.0/255.0;
+                break;
+            case 8:
+                red = 255.0/255.0;
+                green = 102.0/255.0;
+                blue = 0.0/255.0;
+                break;
+            case 9:
+                red = 255.0/255.0;
+                green = 255.0/255.0;
+                blue = 0.0/255.0;
+                break;
+        }
+}
+
+- (IBAction)eraserPressed:(id)sender {
+    self.mainView.image = nil;
+}
+
+- (IBAction)resetPressed:(id)sender {
+    red = 255.0/255.0;
+    green = 255.0/255.0;
+    blue = 255.0/255.0;
+    opacity = 1.0;
+}
 @end
